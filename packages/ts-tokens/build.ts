aeac133b1dbd6e0556f696ca54b5abe376625e71
Adelino Ngomacha <adelinob335@gmail.com>
2026-02-10 14:51:19 +0200

    chore: update build

--- packages/ts-tokens/build.ts
+++ packages/ts-tokens/build.ts
@@ -1,7 +1,7 @@
 import { dts } from 'bun-plugin-dtsx'
 
 const resp = await Bun.build({
-  target: 'browser',
+  target: 'bun',
   entrypoints: ['./src/index.ts'],
   outdir: './dist',
   plugins: [dts()],
